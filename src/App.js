96d631885a40e77398da2bfcd865c6fed4a7dda1
Fausto Palma <faustopalma13@gmail.com>
2022-09-01 17:18:08 -0500

    fix

--- src/App.js
+++ src/App.js
@@ -5,7 +5,10 @@ function App() {
   return (
     <div className='App'>
       <div className='contenedor-calculadora'>
-      
+        <div className='fila'></div>
+        <div className='fila'></div>
+        <div className='fila'></div>
+        <div className='fila'></div>
       </div>
     </div>
   );
