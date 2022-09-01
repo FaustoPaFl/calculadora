47deb4c563bc0374bc700c03c5b98284d6d706ac
Fausto Palma <faustopalma13@gmail.com>
2022-09-01 17:34:58 -0500

    agregando asignacion de clases dinamicas

--- src/componentes/Boton.js
+++ src/componentes/Boton.js
@@ -1,11 +1,17 @@
-import react from 'react'
+import React from 'react'
 
 function Boton(props){
-  return(
-    <div>
+  
+	const esOperador(props.children) = valor => {
+		return isNaN(valor) && (valor != '.') && (valor =! '=')
+	};
+	
+	return(
+    <div 
+		className={`boton-contenedor ${esOperador(props.children) ? 'operador' : null}`}>
 			{props.children}
     </div>
   )
-}
+};
 
 export default Boton
