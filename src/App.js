eb5ae7e49f8d9792b99f7514d2adc446d181f68d
Fausto Palma <faustopalma13@gmail.com>
2022-09-01 17:13:19 -0500

    fix del logo

--- src/App.js
+++ src/App.js
@@ -4,12 +4,9 @@ import freeCodeCampLogo from './imagenes/FreeCodeCamp_logo.png'
 function App() {
   return (
     <div className='App'>
-     <div className='freecodecam-logo-contenedor'>
-      <img
-        src={freeCodeCampLogo}
-        className='freecodecamp-logo'
-        alt='Logo de freeCodeCamp' />
-     </div>
+      <div className='contenedor-calculadora'>
+      
+      </div>
     </div>
   );
 }
