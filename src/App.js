f7d6357a9c0602c4414c45323ca52c195099ec3b
Fausto Palma <faustopalma13@gmail.com>
2022-09-01 17:00:21 -0500

    anadiendo fondo y logo

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import freeCodeCampLogo from './imagenes/FreeCodeCamp_logo.png'
 
 function App() {
   return (
-    <div className="App">
-     
+    <div className='App'>
+     <div className='freecodecam-logo-contenedor'>
+      <img
+        src={freeCodeCampLogo}
+        className='freecodecamp-logo'
+        alt='Logo de freeCodeCamp' />
+     </div>
     </div>
   );
 }
