fb0a865f3135fef41e135ed642c44f3287677ef3
Fausto Palma <faustopalma13@gmail.com>
2022-09-01 18:23:36 -0500

    anadimos componente y boton clear

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
 import Boton from './componentes/Boton';
+import Pantalla from './componentes/Pantalla'
+import BotonClear from './componentes/BotonClear';
 
 function App() {
   return (
     <div className='App'>
       <div className='contenedor-calculadora'>
+        <Pantalla/>
         <div className='fila'>
           <Boton>1</Boton>
           <Boton>2</Boton>
@@ -18,18 +21,20 @@ function App() {
           <Boton>-</Boton>
         </div>
         <div className='fila'>
-        <Boton>7</Boton>
-        <Boton>8</Boton>
-        <Boton>9</Boton>
-        <Boton>*</Boton>
+          <Boton>7</Boton>
+          <Boton>8</Boton>
+          <Boton>9</Boton>
+          <Boton>*</Boton>
         </div>
         <div className='fila'>
-        <Boton>=</Boton>
-        <Boton>0</Boton>
-        <Boton>.</Boton>
-        <Boton>/</Boton>
+          <Boton>=</Boton>
+          <Boton>0</Boton>
+          <Boton>.</Boton>
+          <Boton>/</Boton>
         </div>
-        <div className='fila'></div>
+        <div className='fila'>
+          <BotonClear>Clear</BotonClear>
+        </div>
       </div>
     </div>
   );
---
+++ src/componentes/BotonClear.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const BotonClear = (props.children) => (
+    <div className='boton-clear'>
+        {props.children}
+    </div>
+);
+
+export default BotonClear;
